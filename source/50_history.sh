5645b34fc706025c907709bdb8424b289528be80
Scott Barr <gsbarr@gmail.com>
2019-06-27 17:48:24 +0100

    My history changes

--- source/50_history.sh
+++ source/50_history.sh
@@ -4,15 +4,12 @@
 shopt -s histreedit
 # History expansions will be verified before execution.
 shopt -s histverify
-
+# Append to the history file, don't overwrite it
+shopt -s histappend
 # Entries beginning with space aren't added into history, and duplicate
 # entries will be erased (leaving the most recent entry).
-export HISTCONTROL="ignorespace:erasedups"
-# Give history timestamps.
-export HISTTIMEFORMAT="[%F %T] "
+export HISTCONTROL="ignoreboth"
 # Lots o' history.
-export HISTSIZE=10000
-export HISTFILESIZE=10000
+export HISTSIZE=1000
+export HISTFILESIZE=2000
 
-# Easily re-execute the last history command.
-alias r="fc -s"
